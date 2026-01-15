9f780110b8cf924d17ea0c240db6097ab9080c21
Prajeet22 <prajeetshrivastava6@gmail.com>
2026-01-15 22:32:02 +0530

    first commit

---
+++ backend/config/mongodb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log("Mongo URI:", process.env.MONGO_URI); // TEMP DEBUG
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
